7175606be65c227f2f7b748604bd1288401e9fb0
Remy Spir <r-spir@efluid.fr>
2017-09-21 22:08:46 +0200

    Ajout de la base du code HTML du formulaire

---
+++ index.js
@@ -0,0 +1,10 @@
+import Parseur from './parseur'
+
+/**
+ * fonction appelé lors de la validation du formulaire
+ * @param {Event} event 
+ */
+function validerFormulaire(event) {
+  const {target} = event
+
+}
